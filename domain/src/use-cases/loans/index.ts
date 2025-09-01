4f07a5fafda187228d8120a5c0ba456698043ac4
escobar23 <sofiaescobar97472@gmail.com>
2025-09-01 01:17:49 -0300

    feat: add index files for use case modules

---
+++ domain/src/use-cases/loans/index.ts
@@ -0,0 +1,4 @@
+export * from "./create-loan";
+export * from "./return-loan";
+export * from "./check-loan-expiration";
+export * from "./get-loan-delay-days";
---
+++ domain/src/use-cases/users/index.ts
@@ -0,0 +1,3 @@
+export * from "./login-usuario";
+export * from "./authorize";
+export * from "./get-profile";
