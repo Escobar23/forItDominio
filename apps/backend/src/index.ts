7fbad5934ce4d85a3997c5c5b75ec57237a3babc
escobar23 <sofiaescobar97472@gmail.com>
2025-09-03 21:06:32 -0300

    refactor: rename imports and types from Spanish to English

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,18 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { LoginUsuario } from "../../../domain/src/use-cases/usuarios/login-usuario";
-import { Usuario } from "../../../domain/src/entities/usuarios";
+import {loginUseCase} from "../../../domain/src/use-cases/users/login-user";
+import { User } from "../../../domain/src/entities/users";
 
 const app = express();
 app.use(bodyParser.json());
 
 // repositorio en memoria
-const usuarios: Usuario[] = [
-  { id: "1", nombre: "Sofía", email: "sofia@test.com", contraseñaHash: "1234", rol: "ADMIN" }
+const usuarios: User[] = [
+  { id: "1", name: "Sofía", username: "sofi23", email: "sofia@test.com", hashPassword: "1234", rol: "ADMIN" }
 ];
 
 class InMemoryUsuarioRepo {
-  async findByEmail(email: string): Promise<Usuario | null> {
+  async findByEmail(email: string): Promise<User | null> {
     return usuarios.find(u => u.email === email) || null;
   }
 }
@@ -20,15 +20,12 @@ class InMemoryUsuarioRepo {
 app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const login = new LoginUsuario(new InMemoryUsuarioRepo());
-    const user = await login.execute(email, password);
+
+    const userRepo = new InMemoryUsuarioRepo();
+    const user = await loginUseCase(email, password, userRepo);
 
     res.json({ mensaje: "Login exitoso", user });
   } catch (error: any) {
-    res.status(400).json({ error: error.message });
+    res.status(401).json({ mensaje: error.message });
   }
-});
-
-app.listen(3000, () => {
-  console.log("Backend escuchando en http://localhost:3000");
 });
